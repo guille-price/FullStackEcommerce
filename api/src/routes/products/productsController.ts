8ba03935f964906854ab967f495612f13fc1590b
GuilleNunez <guillermonu817@gmail.com>
2024-11-11 17:43:52 +0800

    Validations with zod + drizzle schema

--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -1,7 +1,8 @@
 import { Request, Response} from 'express'
 import { db } from '../../db/index' 
-import { productsTable } from '../../db/productsSchema'
+import { productsTable, createProductSchema } from '../../db/productsSchema'
 import { eq } from 'drizzle-orm'
+import _ from 'lodash'
 
 export async function listProducts(req: Request, res: Response) {
     try{
@@ -37,7 +38,7 @@ export async function createProduct(req: Request, res: Response) {
     try{
         const [product] = await db
             .insert(productsTable)
-            .values(req.body)
+            .values(req.cleanBody)
             .returning()
         res.status(201).json(product)
     } catch (e) {
@@ -48,7 +49,7 @@ export async function createProduct(req: Request, res: Response) {
 export async function updateProduct(req: Request, res: Response) {
     try{
         const id = Number(req.params.id)
-        const updatedFields = req.body
+        const updatedFields = req.cleanBody
 
         const [product] = await db
             .update(productsTable)
